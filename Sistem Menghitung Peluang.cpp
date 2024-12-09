d968a8f507a49fd66b590eba2a6b92a1114e8a05
viandraagsatri <vagsatri@gmail.com>
2024-12-09 18:35:59 +0700

    Create Sistem Menghitung Peluang.cpp
    Membuat sistem hitung peluang sederhana

---
+++ Sistem Menghitung Peluang.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include <iomanip>
+using namespace std;
+
+double hitungPeluangSederhana(int kejadian, int percobaan){
+  return static_cast<double>(kejadian) / percobaan * 100;
+}
+
+int main (){
+  int pilihan;
+  cout << "Sistem Penghitung Persentase Peluang" << endl;
+  cout << "1. Persentase Peluang Sederhana" << endl;
+  cout << "Pilih jenis peluang (1/): ";
+  cin >> pilihan;
+
+  switch (pilihan){
+    case 1:{
+      int kejadian, percobaan;
+      cout << "Masukkan total kejadian berhasil: ";
+      cin >> kejadian;
+      cout << "Masukkan total percobaan: ";
+      cin >> percobaan;
+      cout << "Persentas peluang: " << fixed << setprecision(2) << hitungPeluangSederhana(kejadian, percobaan) << "%" << endl; 
+      break;  
+    } default:
+      cout << "Pilihan tidak valid!" << endl;
+      break;
+  }
+  
+return 0;
+}
